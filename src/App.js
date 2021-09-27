ef60948f9889ee61c3a1613c51f18d64e8cfe380
Yassine Rassy <47438695+yassineDevox@users.noreply.github.com>
2021-09-27 13:26:18 +0200

    last commit

--- src/App.js
+++ src/App.js
@@ -1,44 +1,84 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import "./App.css";
 import AddTodo from "./components/AddTodo";
 import ListTodo from "./components/ListTodo";
 
 const App = () => {
+  // let listTask = ;
+  // const [listTask, setListTask] = useState(["dazda","eazdaz"])
+  //we will add a backup for our listTask data 
+  const [listTaskBackup, setListTaskBackup] = useState([])
+  const [
+      listTask, 
+        setlistTask
+  ] = useState(["val initial 1", "val initial 2 "])
 
-  const [listTask, setListTask] = useState([])
+  // const filterTaskInput = useRef("")
 
-  const addNewTask = (newTask) => {
-    // alert(newTask)
-    if(newTask!="")
-    setListTask([...listTask,newTask])
-    else alert(" task title should not be empty 😎")
-  }
+  // const addNewTask = (newTask) => {
+  //   // alert(newTask)
+  //   if (newTask != ""){
 
-  //delete task by id
-  const deteleTaskById = (idTask)=>{
+  //     setListTask([...listTask, newTask])
+  //     //add the new value to our backup as well
+  //     setListTaskBackup([...listTaskBackup, newTask])
 
-    if(window.confirm("Are you sure ?")===false) return ;
 
-    let newListTask = [...listTask];
-    newListTask = newListTask.filter((_,index)=>index!=idTask)
-    setListTask([...newListTask])
+  //   }
 
-  }
+  //   else alert(" task title should not be empty 😎")
+  // }
+
+  // //delete task by id
+  // const deteleTaskById = (idTask) => {
+
+  //   if (window.confirm("Are you sure ?") === false) return;
+
+  //   let newListTask = [...listTask];
+  //   newListTask = newListTask.filter((_, index) => index != idTask)
+  //   setListTask([...newListTask])
+  //   //delete the value from our backup as well 
+  //   setListTaskBackup([...newListTask])
+
+  // }
+
+  // //filter tasks by title
+  // const filterTasksByTitle = ()=>{
+  //   let query = filterTaskInput.current.value;
+  //   //test if the query is empty get the backup state 
+  //   if(query==="") setListTask([...listTaskBackup])
+  //   else {
+  //     let newListTask = [...listTask]
+  //     newListTask = newListTask.filter((titleTask)=>titleTask.includes(query))
+  //     setListTask([...newListTask])
+  //   }
+
+  // }
   return (
     <>
       <div>
         {/* addTodo Component */}
-        <AddTodo addTaskToList = { addNewTask } />
+        <AddTodo />
         <hr color="gray" />
+        <button onClick={() => {
+          setlistTask([...listTask,"new value 😅"])
+          console.table(listTask)
+          }}>++</button>
+
+        {/* filter task input */}
         <div className="filter border w-50 mx-auto">
-          <input type="text" placeholder="filter task by title" className="form-control" />
+          <input type="text"
+            placeholder="filter task by title"
+            className="form-control"
+          />
           <i className="fa fa-search" aria-hidden="true" />
         </div>
+
         {/* List Todo Component */}
-        <ListTodo 
+        <ListTodo
           list={listTask}
-          onDeleteTask={deteleTaskById}
-         />
+
+        />
       </div>
     </>
   )
