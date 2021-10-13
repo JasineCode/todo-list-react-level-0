43ad53982b5c9732c7bec1155a97e2020cb4a890
Yassine Rassy <47438695+yassineDevox@users.noreply.github.com>
2021-10-13 13:36:58 +0200

    cdn bootstrap version diff

--- src/index.js
+++ src/index.js
@@ -2,8 +2,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-//bootstrap
-import 'bootstrap/dist/css/bootstrap.css';
+
 
 //component
 import App from './App';
---
+++ src/pages/home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './../styles/home.css'
+
+function HomePage() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default HomePage
