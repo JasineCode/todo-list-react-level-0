a441f19d59ca7e9f2050c26347c4e6f5c188bad0
Yassine Rassy <47438695+yassineDevox@users.noreply.github.com>
2021-10-13 19:08:30 +0200

    life-cycle-bad-components done

--- src/App.js
+++ src/App.js
@@ -1,69 +1,16 @@
 import React, { useState } from 'react'
-import "./App.css"
-import CityAdd from './componets/CityAdd';
-import CityList from './componets/CityList';
-import { City } from './models/city';
+import ClassComp from './components/ClassComp'
 
-
-
-const LIST_CITY = [
-  new City(1,"Casablanca"),
-  new City(2,"Rabat"), 
-]
-
-const App = () => {
-
-  const [cities, setCities] = useState(LIST_CITY)
-
-  const [citiesCopie, setCitiesCopie] = useState(LIST_CITY)
-
-  //add city
-  const addCity = (nameCity) => {
-    let newList = [...cities,new City(cities.length+1,nameCity)]
-    setCities([...newList])
-    setCitiesCopie([...newList])
-
-  }
-
-  //delete city
-  const deleteCityById = (cityId)=>{
-    
-    //are you sure 
-    if(window.confirm("Are you sure ")===false) return  
-    
-    //copie cities
-    let newCities = cities.filter((c)=>c.id!=cityId)
-  
-    //update the state 
-    setCities(newCities) 
-    setCitiesCopie(newCities)
-
-  }
-  const filterCityByName = (e) => {
-
-    if(e.target.value==="") setCities(citiesCopie)
-    else
-    setCities(cities.filter(c=>c.toLowerCase().includes(e.target.value.toLowerCase())))
-  }
-
+function App() {
+  const [show, setShow] = useState(true)
   return (
-
     <div>
-      {/* ADD CITY */}
-      <CityAdd onAddCity={addCity} />
-    
-      <hr />
-      <h1>List Cities</h1>
-      <input 
-          type="search" 
-          placeholder="filter city by name" 
-          onChange={filterCityByName}
-      />
-
-      {/* LIST CITY */}
-      <CityList list={cities} onDeleteList={deleteCityById} />
+      {show == true ? <ClassComp/> : null}
+      <button onClick={()=>setShow(!show)}> {show==true ?'delete':'creer'} component</button>
     </div>
+    
+    
   )
 }
 
-export default App;
+export default App
--- src/models/city.js
+++
@@ -1,7 +0,0 @@
-export class City{
-
-    constructor(id,name){
-        this.id=id
-        this.name=name
-    }
-}
