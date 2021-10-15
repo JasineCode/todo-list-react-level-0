ae3d869bb13ef731aee19f7c3595ac132e65ab41
Yassine Rassy <47438695+yassineDevox@users.noreply.github.com>
2021-10-15 14:18:33 +0200

    redux store done

--- src/index.js
+++ src/index.js
@@ -7,8 +7,7 @@ const initialState = {
 
   todos: [
     new Task(1, "task 1"),
-    new Task(2, "task 2"),
-    new Task(3, "task 3", true),
+    new Task(2, "task 2")
   ]
 }
 
@@ -27,33 +26,28 @@ const TodoReducer = (state = initialState.todos, action = new Action()) => {
 //use of the reducer
 const store = createStore(TodoReducer,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
-store.subscribe(() => console.log(store.getState()))
+
+store.subscribe(()=>console.log(store.getState()))
 
 
+
+  store.dispatch(
+  {
+    type: TYPE_TODO_ADD,
+    payload: new Task(3, "task 3")
+  }
+)
+
 store.dispatch(
   {
-    type:TYPE_TODO_ADD,
-    payload:new Task(
-      store.getState().length + 1,
-      "task" + store.getState().length + 1
-    )
+    type: TYPE_TODO_ADD,
+    payload: new Task(3, "task 3")
   }
 )
 store.dispatch(
   {
-    type:TYPE_TODO_ADD,
-    payload:new Task(
-      store.getState().length + 1,
-      "task" + store.getState().length + 1
-    )
+    type: TYPE_TODO_ADD,
+    payload: new Task(3, "task 3")
   }
 )
-store.dispatch(
-  {
-    type:TYPE_TODO_ADD,
-    payload:new Task(
-      store.getState().length + 1,
-      "task" + store.getState().length + 1
-    )
-  }
-)
+
