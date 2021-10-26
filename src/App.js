9a32482db58cbd5baea38a2654e5bb7f2dab3aa1
Yassine Rassy <47438695+yassineDevox@users.noreply.github.com>
2021-10-26 16:58:20 +0200

    add todo reducer done

--- src/App.js
+++ src/App.js
@@ -1,20 +1,10 @@
 import React from 'react'
-import { Provider } from 'react-redux'
-import TodoAdd from './components/TodoAdd'
-import TodoFilter from './components/TodoFilter'
-import TodoList from './components/TodoList'
-import store from './store'
 
 const App = () => {
     return (
-        <Provider store={store}>
-            <div className="container">
-                <TodoAdd />
-                <hr />
-                <TodoFilter />
-                <TodoList />
-            </div>
-        </Provider>
+        <div>
+            Hello I'm Really Here 🐰 !!
+        </div>
     )
 }
 
--- src/index.js
+++ src/index.js
@@ -1,12 +1,44 @@
-import React from "react";
-import ReactDOM from 'react-dom'
-import App from "./App";
+import { Task } from "./models/task";
 
+const state = [
+  new Task(1, "task 1"),
+  new Task(2, "task 2"),
+  new Task(3, "task 3")
+]
 
+const TodoReducer = (typeAction, inputAction) => {
+  // if (typeAction == "todo/add") {
+  //   return [
+  //     ...state.todos,
+  //     new Task(
+  //       state.todos.length + 1,
+  //       inputAction.titleTask
+  //     )
+  //   ]
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App/>
-  </React.StrictMode>,
-  document.getElementById("root")
-)
+  // }
+  // else if (typeAction == "todo/delete") {
+
+  // }
+  // else if (typeAction == "todo/toggle") {
+
+  // } else {
+  //   return state;
+  // }
+  switch (typeAction) {
+    case "todo/add":
+      return [
+        ...state.todos,
+        new Task(
+          state.todos.length + 1,
+          inputAction.titleTask
+        )
+      ]
+    case "todo/delete":
+      return {}
+    case "todo/toggle":
+      return {}
+    default:
+      return state
+  }
+}
