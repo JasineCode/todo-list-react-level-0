96fda4c7673d5d28f18f9c81ccaf76b0d16a97bf
Yassine Rassy <47438695+yassineDevox@users.noreply.github.com>
2021-10-27 16:45:43 +0200

    we did the same using redux yaay

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import { createStore } from "redux";
 import { Task } from "./models/task";
 
 const initialState = [
@@ -6,12 +7,12 @@ const initialState = [
   new Task(3, "task 3")
 ]
 
-const TYPE_TODO_ADD="todo/add"
+const TYPE_TODO_ADD = "todo/add"
 
-const TodoReducer = (action={},prevState=initialState ) => {
+const TodoReducer = (prevState = initialState, action = {}) => {
 
   switch (action.type) {
-   
+
     case TYPE_TODO_ADD:
       return [
         ...prevState,
@@ -25,21 +26,35 @@ const TodoReducer = (action={},prevState=initialState ) => {
   }
 }
 
-let newState1 = 
-TodoReducer(
+//utilisation 
+const store = createStore(TodoReducer)
+
+// subscribe to store 
+store.subscribe(()=>console.log(store.getState()))
+
+//add new task 4
+store.dispatch(
   {
-    type : TYPE_TODO_ADD,
-    payload : {titleTask:"task 4"}
+    type: TYPE_TODO_ADD,
+    payload: { titleTask: "task 4" }
   }
 )
-console.log(newState1)
 
-let newState2 = 
-TodoReducer(
+
+//add new task 5
+store.dispatch(
   {
-    type : TYPE_TODO_ADD,
-    payload : {titleTask:"task 5"}
-  },
-  newState1
+    type: TYPE_TODO_ADD,
+    payload: { titleTask: "task 5" }
+  }
 )
-console.log(newState2)
+
+
+//add new task 6
+store.dispatch(
+  {
+    type: TYPE_TODO_ADD,
+    payload: { titleTask: "task 6" }
+  }
+)
+
