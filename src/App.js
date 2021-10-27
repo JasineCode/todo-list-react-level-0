aee034b2c5eb4f66bbfe90f83260030bef9f8751
Yassine Rassy <47438695+yassineDevox@users.noreply.github.com>
2021-10-27 13:20:13 +0200

    fetch all data get method done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const App = () => {
+
+    const [data, setData] = useState()
+    const [error, setError] = useState(null)
+    const fetchData = () => {
+        fetch("http://hn.algolia.com/api/v1/items/0")
+        .then(res=>console.log(res))
+        .then(res=>setData(res))
+        .catch(err=>console.log(err))
+    }
+    useEffect(()=>{
+        fetchData()
+    },[])
     return (
         <div>
             Hello I'm Really Here 🐰 !!
--- src/index.js
+++ src/index.js
@@ -1,45 +1,9 @@
-import { Task } from "./models/task";
+import React from "react"
+import ReactDOM from "react-dom"
+import App from "./App"
 
-const initialState = [
-  new Task(1, "task 1"),
-  new Task(2, "task 2"),
-  new Task(3, "task 3")
-]
-
-const TYPE_TODO_ADD="todo/add"
-
-const TodoReducer = (action={},prevState=initialState ) => {
-
-  switch (action.type) {
-   
-    case TYPE_TODO_ADD:
-      return [
-        ...prevState,
-        new Task(
-          prevState.length + 1,
-          action.payload.titleTask
-        )
-      ]
-    default:
-      return prevState
-  }
-}
-
-let newState1 = 
-TodoReducer(
-  {
-    type : TYPE_TODO_ADD,
-    payload : {titleTask:"task 4"}
-  }
-)
-console.log(newState1)
-
-let newState2 = 
-TodoReducer(
-  {
-    type : TYPE_TODO_ADD,
-    payload : {titleTask:"task 5"}
-  },
-  newState1
-)
-console.log(newState2)
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root"))
