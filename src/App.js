28e46e29475cd2de6a34a928d4b23ecd2df27443
Yassine Rassy <47438695+yassineDevox@users.noreply.github.com>
2021-10-27 14:33:35 +0200

    crud done with api

--- src/App.js
+++ src/App.js
@@ -1,21 +1,78 @@
+import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 
 const App = () => {
+    const [todos, setTodos] = useState([])
+    const [task, setTask] = useState()
+    const [updatedTask, setUpdatedTask] = useState({})
 
-    const [data, setData] = useState()
-    const [error, setError] = useState(null)
-    const fetchData = () => {
-        fetch("http://hn.algolia.com/api/v1/items/0")
-        .then(res=>console.log(res))
-        .then(res=>setData(res))
-        .catch(err=>console.log(err))
+    useEffect(() => {
+        axios.get("https://jsonplaceholder.typicode.com/todos/")
+            .then(res => setTodos(res.data))
+    }, [])
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        axios
+            .post("https://jsonplaceholder.typicode.com/todos",
+                {
+                    userId: 1,
+                    id: null,
+                    title: task,
+                    completed: true
+                })
+            .then(res => alert("Task " + res.data.id + " : added successfully 😄"))
     }
-    useEffect(()=>{
-        fetchData()
-    },[])
+
+    const deleteTask = (idTask) => {
+
+        axios
+            .delete("https://jsonplaceholder.typicode.com/todos/" + idTask)
+            .then(res => alert("Task " + idTask + " : deleted 😰"))
+    }
+
+    //server comm
+    const handleSubmitEdit=(e)=>{
+        e.preventDefault()
+        axios.put("https://jsonplaceholder.typicode.com/todos/"+updatedTask.id,
+        updatedTask)
+        .then(res=> alert("task udpated with title : "+res.data.title))
+    }
     return (
         <div>
-            Hello I'm Really Here 🐰 !!
+            <form
+                onSubmit={handleSubmit}
+                className="p-2 text-center">
+                <input type="text" placeholder="title"
+                    onChange={
+                        (e) => setTask(e.target.value)} />
+                <button
+                    type="submit">add task for user 1</button>
+            </form>
+
+            <form
+                onSubmit={handleSubmitEdit}
+                className="p-2 text-center">
+                <input type="text" placeholder="edit task"
+                    onChange={(e) => setUpdatedTask(
+                        {...updatedTask,title:e.target.value}
+                    )} 
+                        value={updatedTask.title}
+                />
+                <button
+                    type="submit">Edit Task </button>
+            </form>
+            <hr />
+            <ul>
+                {todos.filter(t=>t.id<10).map(t =>
+                    <li>{t.title}
+                        <button
+                            onClick={() => deleteTask(t.id)}>delete</button>
+                        <button
+                            onClick={()=> setUpdatedTask(t)}>Edit my Task</button>
+
+                    </li>)}
+            </ul>
         </div>
     )
 }
