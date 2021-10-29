232a9f97158911de824d471488014005b9a2d874
Yassine Rassy <47438695+yassineDevox@users.noreply.github.com>
2021-10-29 18:03:13 +0200

    first method filter using backup list done

--- src/store/Reducer/todo.js
+++ src/store/Reducer/todo.js
@@ -3,53 +3,90 @@ import { TYPE_TODO_ADD, TYPE_TODO_DELETE, TYPE_TODO_FILTER, TYPE_TODO_TOGGLE } f
 
 const initialState = {
 
-    todos:[
+    todos: [
 
         new Task(1, "task 1"),
         new Task(2, "task 2")
-        
-    ],
-    backup:[
 
-    new Task(1, "task 1"),
-    new Task(2, "task 2")
-    
-]
+    ],
+    backup: [
+
+        new Task(1, "task 1"),
+        new Task(2, "task 2")
+
+    ]
 }
 
 
 
-export const ReducerToDo = (prevState=initialState, action) => {
+export const ReducerToDo = (prevState = initialState, action) => {
 
     switch (action.type) {
-        case TYPE_TODO_TOGGLE :
-            return [
-                ...prevState.map(t=>{
-                    if(t.id===action.payload.taskId){
-                        t.isCompleted=!t.isCompleted
-                    }
-                    return t
-                })    
-            ]
+        case TYPE_TODO_TOGGLE: {
 
-        case TYPE_TODO_ADD : 
-            return [
-                ...prevState, new Task(
-                    prevState.length+1,
-                    action.payload.taskTitle
-                )
-            ]
+            let list = prevState.todos.map(t => {
+                if (t.id == action.payload.taskId) {
+                    t.isCompleted = !t.isCompleted
+                }
+                return t
+            })
+
+            return {
+                todos: [...list],
+                backup: [...list]
+            }
+        } break
+
+
+        case TYPE_TODO_ADD:
+            return {
+                todos: [
+                    ...prevState.todos, new Task(
+                        prevState.todos.length + 1,
+                        action.payload.taskTitle
+                    )
+                ],
+                backup: [
+                    ...prevState.todos, new Task(
+                        prevState.length + 1,
+                        action.payload.taskTitle
+                    )
+                ]
+
+            }
         case TYPE_TODO_DELETE:
-            return [
-                ...prevState
-                    .filter((t) => t.id !== action.payload.taskId)]
+            return {
+                todos: [
+                    ...prevState.todos
+                        .filter((t) => t.id !== action.payload.taskId)
+                ],
+                backup: [
+                    ...prevState.todos
+                        .filter((t) => t.id !== action.payload.taskId)
+                ]
+
+            }
         case TYPE_TODO_FILTER:
-            return [
-                ...prevState.filter(
-                    t=>
-                    t.title.includes(action.payload.queryTitle)
-                )
-            ]            
+            {
+                if(action.payload.queryTitle===""){
+                    return {
+                        ...prevState,
+                        todos:[...prevState.backup],
+                        
+                    }
+                }else {
+                    
+                    return {
+                        ...prevState,
+                        todos:[
+                            ...prevState.backup.filter(
+                                t =>
+                                    t.title.includes(action.payload.queryTitle)
+                            )
+                        ]
+                    }
+                }
+            }
 
         default: return prevState
     }
