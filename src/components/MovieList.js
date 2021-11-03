c4386195bb1267046f1f5b9a3d42bc59a6f50b25
Yassine Rassy <47438695+yassineDevox@users.noreply.github.com>
2021-11-03 13:40:25 +0100

    display all animes from the store done

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { connect } from 'react-redux'
+import { useSelector } from 'react-redux'
 import MovieRow from './MovieRow'
 
 const MovieList = ({ movies = [] }) => {
